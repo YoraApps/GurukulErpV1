3f06d180e89be06e51b674671725d74156b6d586
Yogesh Rajput <yogeshrajput335@gmail.com>
2018-09-04 09:46:51 +0530

    DegreeType

--- src/app/pages/settings/degree-type/degree-type.component.ts
+++ src/app/pages/settings/degree-type/degree-type.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component  } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
+
+import { DegreeTypeService } from '../../../pages/settings/data/degree-type.service';
 
 @Component({
   selector: 'ngx-degree-type',
   templateUrl: './degree-type.component.html',
   styleUrls: ['./degree-type.component.scss'],
 })
-export class DegreeTypeComponent implements OnInit {
+export class DegreeTypeComponent {
 
-  constructor() { }
+ settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      Id: {
+        title: 'ID',
+        type: 'number',
+      },
+      DegreeCategoryName: {
+        title: 'DegreeCategory Name',
+        type: 'string',
+      },
+      DegreeTypeCode: {
+        title: 'DegreeType Code',
+        type: 'string',
+      },
+      DegreeTypeName: {
+        title: 'DegreeType Name',
+        type: 'string',
+      },
+      // email: {
+      //   title: 'E-mail',
+      //   type: 'string',
+      // },
+      // age: {
+      //   title: 'Age',
+      //   type: 'number',
+      // },
+    },
+  };
+  source: LocalDataSource = new LocalDataSource();
 
-  ngOnInit() {
+  constructor(private service: DegreeTypeService) {
+    const data = this.service.getData();
+    this.source.load(data);
   }
 
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
 }
--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { SettingsRoutingModule, routedComponents } from './settings-routing.module';
 import { DegreeTypeService } from './data/degree-type.service';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { SmartTableService } from '../../@core/data/smart-table.service';
+
 
 @NgModule({
   imports: [
     ThemeModule,
+    Ng2SmartTableModule,
     SettingsRoutingModule,
   ],
   declarations: [
@@ -14,6 +18,7 @@ import { DegreeTypeService } from './data/degree-type.service';
   ],
   providers: [
     DegreeTypeService,
+    SmartTableService,
   ],
 })
 export class SettingsModule { }
