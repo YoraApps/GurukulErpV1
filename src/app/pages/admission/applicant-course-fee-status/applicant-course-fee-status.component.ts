a1f73e3417cfc0a6c33d0e644c0b113a51c37081
Yogesh Rajput <yogeshrajput335@gmail.com>
2018-09-08 11:38:22 +0530

    admission fee and course fee page added to the application

---
+++ src/app/pages/admission/applicant-course-fee-status/applicant-course-fee-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-applicant-course-fee-status',
+  templateUrl: './applicant-course-fee-status.component.html',
+  styleUrls: ['./applicant-course-fee-status.component.scss']
+})
+export class ApplicantCourseFeeStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
