41cb07ce33c31039e1559c643dfcfa24651528d0
krishnachikam02 <krishna.chikam02@gmail.com>
2018-09-09 19:43:59 +0530

    added campaign

--- src/app/pages/academics/campaign-management/campaign-management.component.ts
+++ src/app/pages/academics/campaign-management/campaign-management.component.ts
@@ -38,13 +38,46 @@ export class CampaignManagementComponent implements OnInit {
       },
     },
   };
+  settingsdata = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      CampaignName: {
+        title: 'Campaign Name',
+        type: 'string',
+      },
+      CityName: {
+        title: 'City Name',
+        type: 'string',
+      },
+      Status: {
+        title: 'Status',
+        type: 'string',
+      },
+    },
+  };
 
   source: LocalDataSource = new LocalDataSource();
+  source1: LocalDataSource = new LocalDataSource();
   degreeTypeList: any[];
 
   constructor(private campaignManagementService: CampaignManagementService) {
     const data = this.campaignManagementService.getData();
+    const data1 = this.campaignManagementService.getAproveData();
     this.source.load(data);
+    this.source1.load(data1);
   }
 
   onDeleteConfirm(event): void {
--- src/app/pages/academics/data/campaign-management.service.ts
+++ src/app/pages/academics/data/campaign-management.service.ts
@@ -12,6 +12,21 @@ export class CampaignManagementService {
     CityName: 'Bhubaneswar',
     FromDate: '	1-2-2019',
     ToDate: '2-2-2013',
+    Status: 'Not Aprroved',
+  }, {
+    CampaignId: 2,
+    CampaignName: 'sdasdasd',
+    CityName: 'Bhubaneswar',
+    FromDate: '	1-2-2019',
+    ToDate: '2-2-2013',
+    Status: 'Aprroved',
+  }];
+  data1 = [{
+    CampaignId: 1,
+    CampaignName: 'sdasdasd',
+    CityName: 'Bhubaneswar',
+    FromDate: '	1-2-2019',
+    ToDate: '2-2-2013',
     Status: 'Aprroved',
   }, {
     CampaignId: 2,
@@ -24,5 +39,8 @@ export class CampaignManagementService {
 
   getData() {
     return this.data;
+  }
+  getAproveData() {
+    return this.data1;
   }
 }
