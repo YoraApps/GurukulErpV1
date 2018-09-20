3c4052ed4194b68174c43b114b426c36a6146592
vipin-cutting-edge <vipin.hari@outlook.com>
2018-09-20 09:12:15 +0530

    menus modified

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -3,13 +3,13 @@ import { NbMenuItem } from '@nebular/theme';
 export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'Dashboard',
-    icon: 'nb-plus-circled',
+    icon: 'nb-grid-a-outline',
     link: '/admission/admission-list',
     home: true,
   },
   {
     title: 'Admission',
-    icon: 'nb-keypad',
+    icon: 'nb-compose',
     children: [
       {
         title: 'Admission list',
@@ -51,7 +51,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
   },
   {
     title: 'Academics',
-    icon: 'nb-keypad',
+    icon: 'nb-tables',
     children: [
       {
         title: 'Memo-Circular-Notice',
@@ -89,7 +89,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
   },
   {
     title: 'Transportation',
-    icon: 'nb-keypad',
+    icon: 'nb-location',
     children: [
       {
         title: 'Location',
@@ -115,7 +115,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
   },
   {
     title: 'Human-Resourse',
-    icon: 'nb-keypad',
+    icon: 'nb-person',
     children: [
       {
         title: 'Staff-Management',
@@ -133,7 +133,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
   },
   {
     title: 'Account',
-    icon: 'nb-keypad',
+    icon: 'nb-bar-chart',
     children: [
       {
         title: 'Account-Summary',
@@ -147,7 +147,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
   },
   {
     title: 'Academic Settings',
-    icon: 'nb-keypad',
+    icon: 'nb-gear',
     children: [
       {
         title: 'Course ',
@@ -213,7 +213,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
   },
   {
     title: 'Manage-Settings',
-    icon: 'nb-keypad',
+    icon: 'nb-grid-b-outline',
     children: [
       {
         title: 'Program Sem Association',
@@ -254,168 +254,6 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Manage Faculty Department',
         link: '/pages/manage-settings/manage-faculty-department',
-      },
-    ],
-  },
-  {
-    title: 'UI Features',
-    icon: 'nb-keypad',
-    link: '/pages/ui-features',
-    children: [
-      {
-        title: 'Buttons',
-        link: '/pages/ui-features/buttons',
-      },
-      {
-        title: 'Grid',
-        link: '/pages/ui-features/grid',
-      },
-      {
-        title: 'Icons',
-        link: '/pages/ui-features/icons',
-      },
-      {
-        title: 'Modals',
-        link: '/pages/ui-features/modals',
-      },
-      {
-        title: 'Popovers',
-        link: '/pages/ui-features/popovers',
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-      {
-        title: 'Tabs',
-        link: '/pages/ui-features/tabs',
-      },
-    ],
-  },
-  {
-    title: 'Forms',
-    icon: 'nb-compose',
-    children: [
-      {
-        title: 'Form Inputs',
-        link: '/pages/forms/inputs',
-      },
-      {
-        title: 'Form Layouts',
-        link: '/pages/forms/layouts',
-      },
-    ],
-  },
-  {
-    title: 'Components',
-    icon: 'nb-gear',
-    children: [
-      {
-        title: 'Tree',
-        link: '/pages/components/tree',
-      }, {
-        title: 'Notifications',
-        link: '/pages/components/notifications',
-      },
-    ],
-  },
-  {
-    title: 'Maps',
-    icon: 'nb-location',
-    children: [
-      {
-        title: 'Google Maps',
-        link: '/pages/maps/gmaps',
-      },
-      {
-        title: 'Leaflet Maps',
-        link: '/pages/maps/leaflet',
-      },
-      {
-        title: 'Bubble Maps',
-        link: '/pages/maps/bubble',
-      },
-      {
-        title: 'Search Maps',
-        link: '/pages/maps/searchmap',
-      },
-    ],
-  },
-  {
-    title: 'Charts',
-    icon: 'nb-bar-chart',
-    children: [
-      {
-        title: 'Echarts',
-        link: '/pages/charts/echarts',
-      },
-      {
-        title: 'Charts.js',
-        link: '/pages/charts/chartjs',
-      },
-      {
-        title: 'D3',
-        link: '/pages/charts/d3',
-      },
-    ],
-  },
-  {
-    title: 'Editors',
-    icon: 'nb-title',
-    children: [
-      {
-        title: 'TinyMCE',
-        link: '/pages/editors/tinymce',
-      },
-      {
-        title: 'CKEditor',
-        link: '/pages/editors/ckeditor',
-      },
-    ],
-  },
-  {
-    title: 'Tables',
-    icon: 'nb-tables',
-    children: [
-      {
-        title: 'Smart Table',
-        link: '/pages/tables/smart-table',
-      },
-    ],
-  },
-  {
-    title: 'Miscellaneous',
-    icon: 'nb-shuffle',
-    children: [
-      {
-        title: '404',
-        link: '/pages/miscellaneous/404',
-      },
-    ],
-  },
-  {
-    title: 'Auth',
-    icon: 'nb-locked',
-    children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
       },
     ],
   },
